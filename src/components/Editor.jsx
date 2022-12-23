87fd76cfe75d86e38d8fcfd6ad3ff5326d303d8f
Sriram <sriramsubramanian23@gmail.com>
2022-12-23 21:19:48 +0530

    Handled tab, added prism.js for syntax highlight

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,21 +1,53 @@
-import React, { useState } from "react"
-import ReactMarkdown from "react-markdown"
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import Prism from "prismjs";
 
-import "./Editor.scss"
+import "./Editor.scss";
+import PLACEHOLDER from "../static/defaultMd";
 
 const Editor = () => {
-    const [md, setMd] = useState();
-    return (
-        <div className="editor-container">
-            <div className="editor">
-                <textarea className="text-area" placeholder="Your markdown content goes here..." onChange={(e) => setMd(e.target.value)} ></textarea>
-            </div>
-            <div className="renderer">
-                <ReactMarkdown>
-                    {md}
-                </ReactMarkdown>
-            </div>
-        </div>
-    )
-}
-export default Editor
+  const spaces = 4
+  const [md, setMd] = useState({value: PLACEHOLDER, caret: -1, target: null});
+  useEffect(() => {
+    Prism.highlightAll();
+  });
+  useEffect(() => {
+
+    if(md.caret >= 0){
+
+        md.target.setSelectionRange(md.caret + spaces, md.caret + spaces);
+
+    }
+
+}, [md]);
+  const handleKeyChange = (e) => {
+    setMd({value: e.target.value, caret: -1, target: e.target})
+  }
+  const handleTab = (e) => {
+    let content = e.target.value;
+    let caret   = e.target.selectionStart;
+    if(e.key === "Tab"){
+      console.log("Tab is pressed")
+      e.preventDefault()
+      let newText = content.substring(0, caret) + ' '.repeat(spaces) + content.substring(caret);
+      setMd({value: newText, caret: caret, target: e.target});
+    }
+  }
+  return (
+    <div className="editor-container">
+      <div className="editor">
+        <textarea
+          className="text-area"
+          placeholder="Your markdown content goes here..."
+          onChange={handleKeyChange}
+          onKeyDown={handleTab}
+          value={md.value}
+        ></textarea>
+      </div>
+      <div className="renderer">
+        <ReactMarkdown>{md.value}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+export default Editor;
---
+++ src/static/defaultMd.js
@@ -0,0 +1,24 @@
+const PLACEHOLDER = `# Hello World!
+
+Welcome to the markdown editor!
+
+\`\`\` Javascript
+import React from "react"
+const MyApp = () => {
+   return <h1>My App</h1>
+}
+export default MyApp
+\`\`\`
+
+\`\`\`shell
+yarn install
+\`\`\`
+
+\`\`\`python
+val = 10
+for i in range(0,val):
+   print(i)
+\`\`\`
+`
+
+export default PLACEHOLDER
